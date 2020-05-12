adecd3b269e8de978d1875ad321010414e20ff01
anikolaidou <anikolaidou@welcomepickups.com>
2020-05-12 13:11:17 +0300

    deleted gxm folder

--- gxm_android/app/src/main/java/com/example/busbuddy/communication/BrokerListMessage.java
+++
@@ -1,9 +0,0 @@
-package com.example.busbuddy.communication;
-
-import java.util.ArrayList;
-import java.util.List;
-
-//koina arxeia me broker, publisher, subscriber
-public class BrokerListMessage {
-    public List<BrokerInfo> brokers = new ArrayList<>();
-}
